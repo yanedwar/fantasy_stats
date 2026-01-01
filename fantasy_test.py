561883a896702db5dcd197d2d7c498e41d8cabe8
Edward <edward929.yan@gmail.com>
2025-12-31 21:10:58 -0500

    Forwards logic implemented

--- fantasy_test.py
+++ fantasy_test.py
@@ -13,30 +13,23 @@ class PlayerStats:
         self.name = name
         self.team = team
         self.position = position
-
+        self.points = 0
         self.games_played = 0
-        self.goals = 0
-        self.assists = 0
-        self.shots = 0
-        self.hits = 0
-        self.blocks = 0
-        self.pm = 0
-        self.takeaways = 0
 
-class GoalieStats:
-    def __init__(self, player_id, name, team):
-        self.id = player_id
-        self.name = name
-        self.team = team
+    def skater(self, goals, assists, shots, sh_goals, hits, blocks, pm, takeaways):
+        self.games_played += 1
+        self.points += 6*goals + 2*sh_goals + 4*assists + 0.9*shots + 2*pm + 0.4*hits + 0.6*blocks + 0.6*takeaways
+        if goals >= 3: #hat trick
+            self.points += 2
+        self.points = round(self.points, 1)
+    
+    def goalie(self, goals, assists, sh_goals, win, otl, shutout, saves, g_against, nine_one):
+        self.games_played += 1
+        self.points += 20*goals + 4*assists + 2*sh_goals + 5*win + 2*otl + 5*shutout + 0.6*saves - 3*g_against + 3*nine_one
+        self.points = round(self.points, 1)
 
-        self.games_played = 0
-        self.goals = 0
-        self.assists = 0
-        self.wins = 0
-        self.saves = 0
-        self.save_pct = 0
-
-
+games_week = []
+players = {}
 
 sch_url = f"https://api-web.nhle.com/v1/schedule/2025-12-21"
 
@@ -50,8 +43,6 @@ if sch_response.status_code != 200 or not sch_response.text.strip():
 
 sch_data = sch_response.json()
 
-games_week = []
-
 for day in sch_data.get("gameWeek", []):
     date = day.get("date", {})
     for game in day.get("games", []):
@@ -64,7 +55,10 @@ for day in sch_data.get("gameWeek", []):
 #for game in games_week:
 games_testing = games_week[:6] #testing so computer doesn't blow up
 
-for game in games_testing:
+
+## TODO: figure out which game is missing - C. McDavid played 3 games in Dec 21-27, but program only recongizes 2. Does it count 21 or 27? 
+
+for game in games_week:
     box_url = f"https://api-web.nhle.com/v1/gamecenter/{game.id}/boxscore"
     box_response = requests.get(box_url, timeout=10)
     print("Status code:", box_response.status_code)
@@ -73,5 +67,31 @@ for game in games_testing:
         exit()
     game_data = box_response.json()
     home_players = game_data.get("playerByGameStats", {}).get("homeTeam")
+    for forward in home_players["forwards"]:
+        player_id = forward.get("playerId")
+        name = forward["name"]["default"]
+        team = game_data.get("homeTeam", {}).get("abbrev")
+        position = forward["position"]
 
+        if player_id not in players:
+            players[player_id] =  PlayerStats(player_id, name, team, position)
+
+        goals = forward.get("goals", 0)
+        assists = forward.get("assists", 0)
+        shots = forward.get("sog", 0)
+        sh_goals = 0 #complete logic later
+        hits = forward.get("hits", 0)
+        blocks = forward.get("blockedShots", 0)
+        pm = forward.get("plusMinus", 0)
+        takeaways = forward.get("takeaways", 0)
+
+        players[player_id].skater(goals, assists, shots, sh_goals, hits, blocks, pm, takeaways)
+
+sorted_players = sorted(players.values(), key=lambda p: p.points, reverse = True)
+top_10 = sorted_players[:10]
+
+for p in top_10:
+    print(f"{p.name} ({p.team}) - {p.points} pts in {p.games_played} games")
+
+print(f"Total number of games this week: {len(games_week)}")
     
